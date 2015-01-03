d805cb5f3c6eb86922b388b586056d22b48172b3
Benjamin Fetters <bfetters14@gmail.com>
2015-01-03 18:39:28 -0500

    ethan fixed

--- colorblast_site/colorblast_site/urls.py
+++ colorblast_site/colorblast_site/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'colorblast_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^colorblastapp/', include('colorblastapp.urls')),
+    url(r'^$', include('colorblastapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
